ec65b5e949e6d9d9be58008698cb9d728fde2250
tamarochka <tamara.zenkevich@gmail.com>
2015-02-23 23:00:01 -0500

    added uploader for a play

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,4 +2,10 @@ class PlaysController < ApplicationController
   def index
     @plays = Play.all
   end
+
+  def upload
+    Play.upload(params[:file])
+    flash[:notice]= "Play Uploaded"
+    redirect_to root_url
+  end
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,3 +1,14 @@
 class Play < ActiveRecord::Base
+  require "rexml/document"
+
+  def self.upload(file)
+    xml_file = File.read(file.path)
+    doc = REXML::Document.new(xml_file)
+    doc.elements.each('PLAY/TITLE') do |element|
+      location = new
+      location.title = element.text
+      location.save!
+    end
+  end
 
 end
